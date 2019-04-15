b3bee46256ca264e68d68d1be0c5a1119ce8b532
harryFBloch <harryfbloch@gmail.com>
2019-04-15 15:54:58 -0400

    added ability to move card around screen with genstures

--- src/Deck.js
+++ src/Deck.js
@@ -1,31 +1,56 @@
 import React, { Component } from 'react'
-import {View, Animated, PanResponder } from 'react-native'
+import {View, Animated, PanResponder, Dimensions } from 'react-native'
+
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 
 export default class Deck extends Component {
 
   constructor(props){
     super(props)
-
+    const position = new Animated.ValueXY()
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gesture) => {console.log(gesture)},
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({x: gesture.dx, y: gesture.dy})
+      },
       onPanResponderRelease: () => {}
     })
 
-    this.state = { panResponder }
+    this.state = { panResponder, position }
+  }
+
+  getCardStyle = () => {
+
+    const { position } = this.state
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    })
+
+    return {
+      ...position.getLayout(),
+      transform: [{rotate: rotate}]
+    }
   }
 
   renderCards = () => {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0){
+        return(
+            <Animated.View key={item.id} style={this.getCardStyle()} {...this.state.panResponder.panHandlers}>
+              {this.props.renderCard(item)}
+            </Animated.View>
+        )
+      }
       return this.props.renderCard(item)
     })
   }
 
   render(){
     return (
-      <View {...this.state.panResponder.panHandlers}>
-      {this.renderCards()}
+      <View >
+        {this.renderCards()}
       </View>
     )
   }
